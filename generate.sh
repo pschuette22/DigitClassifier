3a14306d1fe304dfb2ad3e2eaf028be175adcdd2
pschuette22 <pschuette22@gmail.com>
2022-02-26 16:37:06 -0600

    initial commit

---
+++ generate.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+working_dir=$(pwd)
+output=${working_dir}/dataset
+fonts_dir=${working_dir}/fonts
+rm -rf $output
+mkdir $output
+extensions=("ttf" "otf")
+font_max=100
+
+train_split=7
+valid_split=1
+test_split=2
+total_split=$((train_split + valid_split + test_split))
+
+
+background_colors=("white" "white" "white" "snow1" "snow2" "snow3" "LightPink3" "LavenderBlush1" "LightSlateBlue" "LightSkyBlue1")
+
+for digit in $(seq 0 9); do
+    mkdir -p ${output}/train/${digit}
+    mkdir -p ${output}/valid/${digit}
+    mkdir -p ${output}/test/${digit}
+done
+
+total_fonts=0
+entries=0
+
+for font_zip in ${fonts_dir}/*.zip; do 
+    font_family="$(basename -s .zip $font_zip)"
+    unzip $font_zip -d ${fonts_dir}/${font_family}
+    total_fonts=$((total_fonts+1))
+    
+    for ext in "${extensions[@]}"; do
+        for font in ${fonts_dir}/${font_family}/**.${ext}; do
+            if [[ ! -f "$font" ]]; then continue; fi
+
+            font_name="$(basename -s .$ext $font)"
+
+            # Transformations
+            background=${background_colors[ $RANDOM % ${#background_colors[@]} ]}
+            fill="black"
+
+            split_index=$((entries % total_split))
+            if [ "$split_index" -ge 0 ] && [ "$split_index" -lt "$train_split" ]
+            then 
+                font_split="train"
+            elif [ "$split_index" -ge "$train_split" ] &&  [ "$split_index" -lt $(( $train_split + $valid_split)) ]
+            then 
+                font_split="valid"
+            else
+                font_split="test"
+            fi
+
+            for digit in $(seq 0 9); do
+
+                convert -background $background \
+                        -fill $fill \
+                        -font $font \
+                        -pointsize 300 \
+                        -extent 416x416 \
+                        -gravity center \
+                        label:"$digit" \
+                        ${output}/${font_split}/${digit}/${digit}_${font_name}.jpg
+
+                convert ${output}/${font_split}/${digit}/${digit}_${font_name}.jpg \
+                        -borderColor black \
+                        -border 8 \
+                        ${output}/${font_split}/${digit}/${digit}_${font_name}.jpg
+
+                if [[ $(( entries % 2 )) == 0 ]];
+                then
+                    rotation=$(jot -r 1 -30 30)
+
+                    convert ${output}/${font_split}/${digit}/${digit}_${font_name}.jpg \
+                        -background $background \
+                        -rotate $rotation \
+                        ${output}/${font_split}/${digit}/${digit}_${font_name}_rotated${rotation}.jpg
+                fi
+            done
+
+            entries=$((entries+1))
+
+            # if [ $entries -ge $font_max ]; then break 2; fi
+        done
+    done
+
+    rm -rf ${fonts_dir}/${font_family}
+done
