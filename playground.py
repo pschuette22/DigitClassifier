126ddac35b47eede17722fe1bccbf6da5cbe79a8
pschuette22 <pschuette22@gmail.com>
2024-08-08 10:16:44 -0500

    initial model comparison

--- playground.py
+++ playground.py
@@ -1,80 +1,50 @@
-from tensorflow.keras.preprocessing import image_dataset_from_directory
-import numpy as np
-import tensorflow as tf
-from keras.utils import to_categorical
+import os
+import coremltools as ct
+from PIL import Image
+# from IPython.display import display, Markdown, Latex
 
+apple_model = ct.models.MLModel('MNISTClassifier.mlmodel')
+basic_model = ct.models.MLModel('product/DigitClassifier.mlmodel')
+tuned_model = ct.models.MLModel('product/TunedDigitClassifier.mlmodel')
 
-def print_digit_representation(representation):
-    for x in representation:
-        row = ""
-        for y in x:
-            if y > 0.6:
-                row += "X"
-            elif y > 0.3:
-                row += "-"
+validate_images = 'dataset/fonts/validate'
+
+# Maintian a count of correctly identified images
+models = [("Apple", 'image', apple_model), ("Basic", 'input_1', basic_model), ("Tuned", 'input_1', tuned_model)]
+hits = [0, 0, 0]
+images = 0
+# Iterate over the files in the folder
+for root, dir, files in os.walk(validate_images):
+    for file in files:
+        if not file.endswith('.png') and not file.endswith('.jpg'):
+            # Not an image
+            continue
+        actual_digit = int(root.split('/')[-1])
+        file_path = os.path.join(root, file)
+        
+        # Load the image in grayscale
+        image = Image.open(file_path).convert('L')
+        images += 1
+        model_misses = 0
+        for model in models:
+            # Make a prediction
+            prediction = model[2].predict({model[1]: image})
+            digit = int(prediction['classLabel'])
+            # Check if the prediction is correct
+            if digit == actual_digit:
+                hits[models.index(model)] += 1
+                # print(f"{model[0]} predicted {digit} correctly")
             else:
-                row += " "
-        print(row)
+                # Get the predicted digit
+                # print(prediction)
+                print(f'Predicted: {digit}')
+                # Get the actual digit
+                print(f'Actual: {actual_digit}')
+                print(f"{model[0]} predicted {digit} incorrectly ({file_path})")
+                model_misses += 1
+        if model_misses == 3:
+            print(f"All models missed {file_path}")
 
-# Import font data
-font_training_data = 'dataset/fonts/train'
-font_test_dataset = 'dataset/fonts/test'
-
-# Load the dataset from the fonts directory
-fonts_train_dataset = image_dataset_from_directory(
-    font_training_data,
-    labels='inferred',
-    label_mode='categorical',
-    color_mode='grayscale',
-    batch_size=32,
-    image_size=(28, 28),
-    shuffle=False
-)
-
-fonts_test_dataset = image_dataset_from_directory(
-    font_test_dataset,
-    labels='inferred',
-    label_mode='categorical',
-    color_mode='grayscale',
-    batch_size=32,
-    image_size=(28, 28),
-    shuffle=False
-)
-
-# Normalize the images to the [0, 1] range
-normalization_layer = tf.keras.layers.Rescaling(1./255)
-fonts_train_dataset = fonts_train_dataset.map(lambda x, y: (normalization_layer(x), y))
-fonts_test_dataset = fonts_test_dataset.map(lambda x, y: (normalization_layer(x), y))
-
-# # Convert the labels to categorical
-# fonts_train_dataset = fonts_train_dataset.map(lambda x, y: (x, to_categorical(y, 10)))
-# fonts_test_dataset = fonts_test_dataset.map(lambda x, y: (x, to_categorical(y, 10)))
-
-
-# Combine the fonts dataset with the existing MNIST dataset
-x_train_fonts = np.concatenate([x for x, y in fonts_train_dataset])
-y_train_fonts = np.concatenate([y for x, y in fonts_train_dataset])
-y_train_fonts.astype(np.uint8)
-
-x_test_fonts = np.concatenate([x for x, y in fonts_test_dataset])
-y_test_fonts = np.concatenate([y for x, y in fonts_test_dataset])
-y_test_fonts.astype(np.uint8)
-
-print("Font data example")
-i = 200
-while i < x_train_fonts.shape[0]:
-    print_digit_representation(x_train_fonts[i])
-    print(y_train_fonts[i])
-    i += 1000
-
-i = 100
-while i < x_test_fonts.shape[0]:
-    print_digit_representation(x_test_fonts[i])
-    print(y_test_fonts[i])
-    i += 800
-
-# Print the shapes of the combined datasets
-print("Combined x_train shape:", x_train_fonts.shape)
-print("Combined y_train shape:", y_train_fonts.shape)
-print("Combined x_test shape:", x_test_fonts.shape)
-print("Combined y_test shape:", y_test_fonts.shape)
+print(f"Apple: {hits[0]} out of {images}: {hits[0] / images}")
+print(f"Basic: {hits[1]} out of {images}: {hits[1] / images}")
+print(f"Tuned: {hits[2]} out of {images}: {hits[2] / images}")
